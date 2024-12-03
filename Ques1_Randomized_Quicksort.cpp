829091f640d560ae58959c212f4053c311783e16
Sarthak14278 <polly.gray.hu.main@gmail.com>
2024-12-03 10:55:52 -0800

    Update Ques1_Randomized_Quicksort.cpp

--- Ques1_Randomized_Quicksort.cpp
+++ Ques1_Randomized_Quicksort.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
 using namespace std;
 
 int comparisons = 0; 
@@ -12,22 +11,36 @@ void swap(int* a, int* b) {
 }
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
+    int pivot = arr[low]; // Use the first element as the pivot
+    int start = low;
+    int end = high;
 
-    for (int j = low; j <= high - 1; j++) {
-        comparisons++; 
-        if (arr[j] <= pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
+    while (start < end) {
+        // Increment start while arr[start] <= pivot
+        while (arr[start] <= pivot && start < high) {
+            comparisons++;
+            start++;
+        }
+
+        // Decrement end while arr[end] > pivot
+        while (arr[end] > pivot && end > low) {
+            comparisons++;
+            end--;
+        }
+
+        // Swap elements if start < end
+        if (start < end) {
+            swap(&arr[start], &arr[end]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+
+    // Swap the pivot with arr[end]
+    swap(&arr[low], &arr[end]);
+    return end; // Return the pivot's final position
 }
 
+
 int partition_r(int arr[], int low, int high) {
-    srand(time(NULL));
     int random = low + rand() % (high - low + 1); 
     swap(&arr[random], &arr[high]);
     return partition(arr, low, high);
@@ -66,4 +79,5 @@ int main() {
 
     return 0;
 }
+
 
